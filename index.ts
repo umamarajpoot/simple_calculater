5f7316076dff60ce44899020b7479e6cd465b664
umamarajpoot <164630754+umamarajpoot@users.noreply.github.com>
2024-03-26 09:52:12 +0500

    push my calculater to github

---
+++ index.ts
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    {message: "Enter your first number", type: "number", name: "firstNumber" },
+    {message: "Enter your second number", type: "number", name: "secondNumber" },
+    {message: "Select one of operater to porfome action", type:"list", name:"operater",choices:
+["ADDITION", "SUBTRACTION", "MULTLIPICATION", "DIVISION"],},
+]);
+// CONDITIONAL STATMENT
+if(answer.operater === "ADDITION"){
+    console.log(answer.firstNumber + answer.secondNumber) ;
+}
+else if(answer.operater === "SUBTRACTION"){
+    console.log(answer.firstNumber - answer.secondNumber) ;
+}
+else if(answer.operater === "MULTLIPICATION"){
+    console.log(answer.firstNumber * answer.secondNumber) ;
+}else if(answer.operater === "DIVISION"){
+    console.log(answer.firstNumber / answer.secondNumber) ;   
+}
+else{"please select correct operater "}
+ 
